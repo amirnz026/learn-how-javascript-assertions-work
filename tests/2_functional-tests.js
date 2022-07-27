343eadc58af2eee1bcc39b128ea77fa70a4c159e
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-27 15:47:00 +0400

    hello world

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -30,10 +30,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=amir')
+        .get('/hello?name=John')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, `hello ${res.query.name}`);
+          assert.equal(res.status, 200);
+          assert.equal(res.text, `hello John`);
           done();
         });
     });
